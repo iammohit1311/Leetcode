a02af175935766a962db03d0c05c73c8b4b7bae2
Mohit Srivastava <68652410+iammohit1311@users.noreply.github.com>
2022-02-20 15:09:55 +0530

    Create intersectionOfTwoLL.cpp

---
+++ intersectionOfTwoLL.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==NULL || headB==NULL) return NULL;
+        ListNode* slow=headA;
+        ListNode* fast=headB;
+        
+        while(slow != fast)
+        {
+            slow = slow==NULL ? headB : slow->next;
+            fast = fast==NULL ? headA : fast->next;
+        }
+        return slow;
+    }
+};
