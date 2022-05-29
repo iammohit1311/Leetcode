b8abaed642dfcad9bbc7f67f28d11ec6b1670625
Mohit Srivastava <iammohitpro@gmail.com>
2022-05-29 23:45:22 +0530

    Time: 191 ms (75.80%), Space: 102.6 MB (84.91%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int ans;
+class NodeValue {
+public:
+    int maxNode, minNode, maxSize;
+    
+    NodeValue(int minNode, int maxNode, int maxSize) {
+        this->maxNode = maxNode;
+        this->minNode = minNode;
+        this->maxSize = maxSize;
+    }
+};
+
+class Solution {
+private:
+    NodeValue largestBSTSubtreeHelper(TreeNode* root) {
+        // An empty tree is a BST of size 0.
+        if (!root) {
+            return NodeValue(INT_MAX, INT_MIN, 0);
+        }
+        
+        // Get values from left and right subtree of current tree.
+        auto left = largestBSTSubtreeHelper(root->left);
+        auto right = largestBSTSubtreeHelper(root->right);
+        
+        // Current node is greater than max in left AND smaller than min in right, it is a BST.
+        if (left.maxNode < root->val && root->val < right.minNode) {
+            // It is a BST.
+            ans = max(ans, left.maxSize + right.maxSize + root->val);
+            return NodeValue(min(root->val, left.minNode), max(root->val, right.maxNode), 
+                            left.maxSize + right.maxSize + root->val);
+        }
+        
+        // Otherwise, return [-inf, inf] so that parent can't be valid BST
+        return NodeValue(INT_MIN, INT_MAX, max(left.maxSize, right.maxSize));
+    }
+    public:
+    int maxSumBST(TreeNode* root) {
+        ans = 0;
+        largestBSTSubtreeHelper(root);
+        return ans > 0 ? ans : 0;
+    }
+};
